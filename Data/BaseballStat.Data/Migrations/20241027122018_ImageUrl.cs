45582b4286aa68ee5654d706beb40ed49b6f4282
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-10-27 14:36:39 +0200

    create LeagueSeeder

---
+++ Data/BaseballStat.Data/Migrations/20241027122018_ImageUrl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaseballStat.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Leagues",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Leagues");
+        }
+    }
+}
---
+++ Data/BaseballStat.Data/Seeding/CustomSeeder/LeagueSeeder.cs
@@ -0,0 +1,43 @@
+﻿namespace BaseballStat.Data.Seeding.CustomSeeder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using BaseballStat.Data.Models;
+
+    public class LeagueSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Leagues.Any())
+            {
+                return;
+            }
+
+            var leagues = new League[]
+            {
+                new League
+                {
+                    Name = "American League",
+
+                    ImageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/5/54/American_League_logo.svg/800px-  American_League_logo.svg.png",
+
+                },
+                new League
+                {
+                    Name = "National League",
+
+                    ImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdN8IBk91qrItuyZFGy7nGGCk9BP1HJNR-zg&s",
+                },
+            };
+            foreach (var league in leagues)
+            {
+                await dbContext.AddAsync(league);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
