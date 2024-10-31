b8d3e150b8de1982f5a4b262c5eb7bfaffecdcb2
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-10-31 12:44:27 +0200

    Create Category Service

---
+++ Services/BaseballStat.Services.Data/Categories/CategoriesService.cs
@@ -0,0 +1,68 @@
+﻿namespace BaseballStat.Services.Data.Categories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using BaseballStat.Data.Common.Repositories;
+    using BaseballStat.Data.Models;
+    using BaseballStat.Services.Mapping;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CategoriesService : ICategoriesService
+    {
+        private readonly IDeletableEntityRepository<Category> categoriesRepository;
+
+        public CategoriesService(IDeletableEntityRepository<Category> catеgoriesRepository)
+        {
+            this.categoriesRepository = catеgoriesRepository;
+        }
+
+        public async Task AddAsync(string name, string description, string imageUrl)
+        {
+            await this.categoriesRepository.AddAsync(new Category
+            {
+                Name = name,
+                Description = description,
+                ImageUrl = imageUrl,
+            });
+            await this.categoriesRepository.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var category =
+                this.categoriesRepository
+                .AllAsNoTracking()
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+            this.categoriesRepository.Delete(category);
+            await this.categoriesRepository.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync<T>(int? count = null)
+        {
+            IQueryable<Category> query = this.categoriesRepository
+                .All()
+                .OrderBy(x => x.Name);
+            if (count.HasValue)
+            {
+                query = query.Take(count.Value);
+            }
+
+            return await query.To<T>().ToListAsync();
+        }
+
+        public Task<T> GetByIdAsync<T>(int id)
+        {
+            var category = this.categoriesRepository
+                .All()
+                .Where(x => x.Id == id)
+                .To<T>()
+                .FirstOrDefaultAsync();
+            return category;
+        }
+    }
+}
---
+++ Services/BaseballStat.Services.Data/Categories/ICategoriesService.cs
@@ -0,0 +1,19 @@
+﻿namespace BaseballStat.Services.Data.Categories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface ICategoriesService 
+    {
+        Task<IEnumerable<T>> GetAllAsync<T>(int? count = null);
+
+        Task<T> GetByIdAsync<T>(int id);
+
+        Task AddAsync(string name, string description, string imageUrl);
+
+        Task DeleteAsync(int id);
+    }
+}
