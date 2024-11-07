461656b0956bfb1ddcf1c04fd6ecd7a683ed1f42
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-07 12:41:02 +0200

    work on playerviewmodels

---
+++ Web/BaseballStat.Web.ViewModels/Player/PlayerViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace BaseballStat.Web.ViewModels.Player
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using BaseballStat.Common;
+    using BaseballStat.Data.Models;
+    using BaseballStat.Services.Mapping;
+
+    public class PlayerViewModel : IMapFrom<Player>
+    {
+        [Required]
+        [MaxLength(GlobalConstants.DataValidations.FirstNameMaxLength)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(GlobalConstants.DataValidations.LastNameMaxLength)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+
+        [Required]
+        public string Bats { get; set; }
+
+        [Required]
+        public string Throws { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Range(GlobalConstants.DataValidations.YearOfBirthMinValue, GlobalConstants.DataValidations.YearOfBirthMaxValue)]
+        public int YearOfBirth { get; set; }
+
+        public int TeamId { get; set; }
+    }
+}
