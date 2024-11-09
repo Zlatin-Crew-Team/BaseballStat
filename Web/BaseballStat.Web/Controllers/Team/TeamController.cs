9b853464c4835e99120c7f7df9b2c1e7098d34d4
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-09 13:56:59 +0200

    work on team controller

--- Web/BaseballStat.Web/Controllers/Team/TeamController.cs
+++ Web/BaseballStat.Web/Controllers/Team/TeamController.cs
@@ -1,6 +1,29 @@
 ﻿namespace BaseballStat.Web.Controllers.TeamController
 {
-    public class TeamController
+    using System.Threading.Tasks;
+
+    using BaseballStat.Services.Data.Teams;
+    using BaseballStat.Web.ViewModels.Player;
+    using BaseballStat.Web.ViewModels.Team;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TeamController : BaseController
     {
+        private readonly ITeamService teamService;
+
+        public TeamController(ITeamService teamService)
+        {
+            this.teamService = teamService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new TeamListViewModel
+            {
+                Teams = await this.teamService.GetAllTeamsAsync<TeamViewModel>(),
+            };
+
+            return this.View(viewModel);
+        }
     }
 }
