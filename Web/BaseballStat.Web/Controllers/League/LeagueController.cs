cc1a67014cdc6483fd1f27f8ddbf6384588eb70a
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-10 12:06:28 +0200

    work on LeagueController

---
+++ Web/BaseballStat.Web/Controllers/League/LeagueController.cs
@@ -0,0 +1,29 @@
+﻿namespace BaseballStat.Web.Controllers.League
+{
+    using System.Threading.Tasks;
+
+    using BaseballStat.Services.Data.League;
+    using BaseballStat.Web.ViewModels.League;
+    using BaseballStat.Web.ViewModels.Player;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class LeagueController : BaseController
+    {
+        private readonly ILeagueService leagueService;
+
+        public LeagueController(ILeagueService leagueService)
+        {
+            this.leagueService = leagueService;
+        }
+
+        public async Task<IActionResult> Index(int id)
+        {
+            var viewModel = new LeagueListViewModel
+            {
+                Leagues = await this.leagueService.GetAllLeaguesAsync<LeagueViewModel>(),
+            };
+
+            return this.View(viewModel);
+        }
+    }
+}
