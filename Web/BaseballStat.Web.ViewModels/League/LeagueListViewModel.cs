5662a1cb30d1b4284aed89fe0895acbb7d4a8e44
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-10 12:07:11 +0200

    work on League ViewModels

---
+++ Web/BaseballStat.Web.ViewModels/League/LeagueListViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BaseballStat.Web.ViewModels.League
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LeagueListViewModel
+    {
+        public IEnumerable<LeagueViewModel> Leagues { get; set; }
+    }
+}
---
+++ Web/BaseballStat.Web.ViewModels/League/LeagueViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BaseballStat.Web.ViewModels.League
+{
+    using System.Collections;
+
+    using BaseballStat.Data.Models;
+    using BaseballStat.Services.Mapping;
+
+    public class LeagueViewModel : IMapFrom<League>
+    {
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
