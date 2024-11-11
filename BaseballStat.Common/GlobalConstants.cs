a81db7acc7f72ff978d394eef4fabb252ab481a5
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-11 18:42:44 +0200

    work on GlobalConstants

--- BaseballStat.Common/GlobalConstants.cs
+++ BaseballStat.Common/GlobalConstants.cs
@@ -52,6 +52,37 @@
             public const int FoundedYearMaxValue = 2020;
         }
 
+        public static class PlayerStatistic
+        {
+            public const int GamesMinValue = 0;
+
+            public const int GamesMaxValue = 4000;
+
+            public const int AtBatsMinValue = 0;
+
+            public const int AtBatsMaxValue = 20000;
+
+            public const int RunsMinValue = 0;
+
+            public const int RunsMaxValue = 4000;
+
+            public const int HitsMinValue = 0;
+
+            public const int HitsMaxValue = 4280;
+
+            public const int DoublesMinValue = 0;
+
+            public const int DoublesMaxValue = 1800;
+
+            public const int TriplesMinValue = 0;
+
+            public const int TriplesMaxValue = 700;
+
+            public const int HomeRunsMinValue = 0;
+
+            public const int HomeRunsMaxValue = 800;
+        }
+
         public static class ErrorMesages
         {
             public const string Name = "Name must be between 2 and 40 characters.";
@@ -59,6 +90,19 @@
             public const string Description = "Description must be between 5 and 100 characters.";
 
             public const string Image = "Please select a JPG, JPEG or PNG image smaller than 1MB.";
+
+            public const string Games = "Games must be between 0 and 4000.";
+
+            public const string AtBats = "At Bats must be between 0 and 20000.";
+
+            public const string Runs = "Runs must be between 0 and 4000.";
+
+            public const string Hits = "Hits must be between 0 and 4280.";
+            public const string Doubles = "Doubles must be between 0 and 1800.";
+
+            public const string Triples = "Triples must be between 0 and 700.";
+
+            public const string HomeRuns = "Home Runs must be between 0 and 800.";
         }
 
         public static class Images
