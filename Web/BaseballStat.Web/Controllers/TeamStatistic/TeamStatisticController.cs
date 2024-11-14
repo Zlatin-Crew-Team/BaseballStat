a8b6b4f4c2c47b9c096ea6ceb605bb04513149c0
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-14 13:42:01 +0200

    create TeamStatisticController

---
+++ Web/BaseballStat.Web/Controllers/TeamStatistic/TeamStatisticController.cs
@@ -0,0 +1,22 @@
+﻿namespace BaseballStat.Web.Controllers.TeamStatistic
+{
+    using BaseballStat.Services.Data.TeamStatistic;
+    using BaseballStat.Web.ViewModels.TeamStatistic;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class TeamStatisticController : Controller
+    {
+        private readonly ITeamStatisticService teamStatisticService;
+
+        public TeamStatisticController(ITeamStatisticService teamStatisticService)
+        {
+            this.teamStatisticService = teamStatisticService;
+        }
+
+        public IActionResult Index()
+        {
+            var team = this.teamStatisticService.GetAllTeamStatisticsAsync<TeamStatisticViewModel>();
+            return this.View(team);
+        }
+    }
+}
