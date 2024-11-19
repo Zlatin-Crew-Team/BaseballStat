465f10367b839f22e15e16afaf0aa3d244032ece
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-19 19:07:00 +0200

    create ILeagueService

---
+++ Services/BaseballStat.Services.Data/LeagueStatistic/ILeagueStatisticService.cs
@@ -0,0 +1,15 @@
+﻿namespace BaseballStat.Services.Data.LeagueStatistic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface ILeagueStatisticService
+    {
+        Task<T> GetByIdAsync<T>(int id);
+
+        Task<T> GetLeagueStatisticByIdAsync<T>(int id);
+    }
+}
---
+++ Services/BaseballStat.Services.Data/LeagueStatistic/LeagueStatisticService.cs
@@ -0,0 +1,43 @@
+﻿namespace BaseballStat.Services.Data.LeagueStatistic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using BaseballStat.Data.Common.Repositories;
+    using BaseballStat.Data.Models;
+    using BaseballStat.Services.Mapping;
+    using Microsoft.EntityFrameworkCore;
+
+    public class LeagueStatisticService : ILeagueStatisticService
+    {
+        private readonly IRepository<LeagueStatistic> leagueStatistics;
+
+        public LeagueStatisticService(IRepository<LeagueStatistic> leagueStatistics)
+        {
+            this.leagueStatistics = leagueStatistics;
+        }
+
+        public async Task<T> GetByIdAsync<T>(int id)
+        {
+            var leagueStatistic = await this.leagueStatistics
+                .All()
+                .Where(x => x.Id == id)
+                .To<T>()
+                .FirstOrDefaultAsync();
+            return leagueStatistic;
+        }
+
+        public async Task<T> GetLeagueStatisticByIdAsync<T>(int id)
+        {
+            var leagueStatistic = await this.leagueStatistics
+                .All()
+                .Where(x => x.Id == id)
+                .To<T>()
+                .FirstOrDefaultAsync();
+            return leagueStatistic;
+        }
+    }
+}
