69cded8c1a7275a132858b5863e4183caf7ab455
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-19 19:08:14 +0200

    create viewModels and View LeagueStatistic

---
+++ Web/BaseballStat.Web.ViewModels/LeagueStatistic/LeagueStatisticListViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BaseballStat.Web.ViewModels.LeagueStatistic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LeagueStatisticListViewModel
+    {
+        public IEnumerable<LeagueStatisticViewModel> LeagueStatistics { get; set; }
+    }
+}
