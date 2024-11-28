41a8d1e13a8f2c1eaecb5d6736d0f7585f388ed3
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-28 08:48:29 +0200

    create awardController

---
+++ Web/BaseballStat.Web/Controllers/Award/AwardController.cs
@@ -0,0 +1,28 @@
+﻿namespace BaseballStat.Web.Controllers.Award
+{
+    using System.Threading.Tasks;
+
+    using BaseballStat.Services.Data.Award;
+    using BaseballStat.Web.ViewModels.Award;
+    using BaseballStat.Web.ViewModels.Category;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AwardController : BaseController
+    {
+        private readonly IAwardService awardService;
+
+        public AwardController(IAwardService awardService)
+        {
+            this.awardService = awardService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new AwardListViewModel
+            {
+                Awards = await this.awardService.GetAllAsync<AwardViewModel>(),
+            };
+            return this.View(viewModel);
+        }
+    }
+}
