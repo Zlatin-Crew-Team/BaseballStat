de655a6aef061c5d4ea7cd1982bb113869dc4b68
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-28 13:21:21 +0200

    create pagination in awardController

--- Web/BaseballStat.Web/Controllers/Award/AwardController.cs
+++ Web/BaseballStat.Web/Controllers/Award/AwardController.cs
@@ -1,10 +1,13 @@
 ﻿namespace BaseballStat.Web.Controllers.Award
 {
+    using System;
+    using System.Linq;
     using System.Threading.Tasks;
 
     using BaseballStat.Services.Data.Award;
     using BaseballStat.Web.ViewModels.Award;
     using BaseballStat.Web.ViewModels.Category;
+    using BaseballStat.Web.ViewModels.Player;
     using Microsoft.AspNetCore.Mvc;
 
     public class AwardController : BaseController
@@ -16,11 +19,17 @@
             this.awardService = awardService;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int pageIndex = 1, int pageSize = 2)
         {
+            var awardedPlayers = await this.awardService.GetAllAwardAsync<AwardViewModel>();
+            var count = awardedPlayers.Count();
+            var items = awardedPlayers.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+
             var viewModel = new AwardListViewModel
             {
-                Awards = await this.awardService.GetAllAsync<AwardViewModel>(),
+                Awards = items,
+                PageIndex = pageIndex,
+                TotalPages = (int)Math.Ceiling(count / (double)pageSize),
             };
             return this.View(viewModel);
         }
