26d2766bc9b65ce7316e933cd62f589e0db7f709
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-29 12:35:37 +0200

    create ViewModelRecord

---
+++ Web/BaseballStat.Web.ViewModels/Record/RecordViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BaseballStat.Web.ViewModels.Record
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using BaseballStat.Data.Models;
+    using BaseballStat.Services.Mapping;
+
+    public class RecordViewModel : IMapFrom<Record>
+    {
+        [Required]
+        public string Holder { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+    }
+}
