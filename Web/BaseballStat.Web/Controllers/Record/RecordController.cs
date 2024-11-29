1215f3f732b1ac4b8615ef4c0d7a9db70370f447
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-29 12:34:56 +0200

    create RecordController and view

---
+++ Web/BaseballStat.Web/Controllers/Record/RecordController.cs
@@ -0,0 +1,26 @@
+﻿namespace BaseballStat.Web.Controllers.Record
+{
+    using System.Threading.Tasks;
+
+    using BaseballStat.Services.Data.Records;
+    using BaseballStat.Web.ViewModels.Record;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class RecordController : BaseController
+    {
+        private readonly IRecordService recordService;
+
+        public RecordController(IRecordService recordService)
+        {
+            this.recordService = recordService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = await
+                this.recordService.
+                GetAllRecordsAsync<RecordViewModel>();
+            return this.View(viewModel);
+        }
+    }
+}
