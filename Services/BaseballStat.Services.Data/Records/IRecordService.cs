783f21f55ee268bde6555ce6d8f21aae8b25070e
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-29 12:34:29 +0200

    create RecordService

---
+++ Services/BaseballStat.Services.Data/Records/IRecordService.cs
@@ -0,0 +1,15 @@
+﻿namespace BaseballStat.Services.Data.Records
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IRecordService
+    {
+        Task<IEnumerable<T>> GetAllRecordsAsync<T>(int? count = null);
+
+        Task<T> GetRecordByIdAsync<T>(int id);
+    }
+}
---
+++ Services/BaseballStat.Services.Data/Records/RecordService.cs
@@ -0,0 +1,44 @@
+﻿namespace BaseballStat.Services.Data.Records
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using BaseballStat.Data.Common.Repositories;
+    using BaseballStat.Data.Models;
+    using BaseballStat.Services.Mapping;
+    using Microsoft.EntityFrameworkCore;
+
+    public class RecordService : IRecordService
+    {
+        private readonly IDeletableEntityRepository<Record> recordsRepository;
+
+        public RecordService(IDeletableEntityRepository<Record> recordsRepository)
+        {
+            this.recordsRepository = recordsRepository;
+        }
+
+        public async Task<IEnumerable<T>> GetAllRecordsAsync<T>(int? count = null)
+        {
+            IQueryable<Record> query = this.recordsRepository
+                .All()
+                .OrderBy(x => x.Id);
+            if (count.HasValue)
+            {
+                query = query.Take(count.Value);
+            }
+
+            return await query.To<T>().ToListAsync();
+        }
+
+        public Task<T> GetRecordByIdAsync<T>(int id)
+        {
+            var record = this.recordsRepository
+                .All()
+                .Where(x => x.Id == id)
+                .To<T>()
+                .FirstOrDefaultAsync();
+            return record;
+        }
+    }
+}
