8b3ce34f1c4b7be5178fab0634fcaf22eabb2784
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-11-30 16:06:46 +0200

    create record ViewModek Controller and view

---
+++ Web/BaseballStat.Web.ViewModels/Record/RecordInputModel.cs
@@ -0,0 +1,39 @@
+﻿namespace BaseballStat.Web.ViewModels.Record
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using BaseballStat.Common;
+    using BaseballStat.Web.ViewModels.Common.CustomValidationAttributes;
+    using Microsoft.AspNetCore.Http;
+
+    public class RecordInputModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(GlobalConstants.DataValidations.HolderMaxLength, MinimumLength = GlobalConstants.DataValidations.HolderMinLength)]
+        public string Holder { get; set; }
+
+        [Required]
+        [StringLength(GlobalConstants.DataValidations.BioMaxLength, MinimumLength = GlobalConstants.DataValidations.BioMinLength)]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Upload)]
+        [ValidateImageFileAttribute(ErrorMessage = GlobalConstants.ErrorMesages.Image)]
+        public IFormFile Image { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [Range(GlobalConstants.DataValidations.AwardTypeIdMinValue, GlobalConstants.DataValidations.AwardTypeIdMaxValue)]
+        public int RecordTypeId { get; set; }
+    }
+}
---
+++ Web/BaseballStat.Web/Areas/Administration/Controllers/Record/RecordController.cs
@@ -0,0 +1,81 @@
+﻿namespace BaseballStat.Web.Areas.Administration.Controllers.Record
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using BaseballStat.Common;
+    using BaseballStat.Services.Cloudinary;
+    using BaseballStat.Services.Data.Records;
+    using BaseballStat.Web.ViewModels.AllTimeGreat;
+    using BaseballStat.Web.ViewModels.Record;
+    using CloudinaryDotNet;
+    using CloudinaryDotNet.Actions;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Area("Administration")]
+    public class RecordController : AdministrationController
+    {
+        private readonly IRecordService recordService;
+        private readonly ICloudinaryService cloudinaryService;
+
+        public RecordController(IRecordService recordService, ICloudinaryService cloudinaryService)
+        {
+            this.recordService = recordService;
+            this.cloudinaryService = cloudinaryService;
+        }
+
+        public async Task<IActionResult> Index(int pageIndex = 1, int pageSize = 3)
+        {
+            var record = await this.recordService.GetAllRecordsAsync<RecordViewModel>();
+            var count = record.Count();
+            var items = record.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+
+            var viewModel = new RecordListViewModel
+            {
+                Record = items,
+                PageIndex = pageIndex,
+                TotalPages = (int)Math.Ceiling(count / (double)pageSize),
+            };
+            return this.View(viewModel);
+        }
+
+        public IActionResult AddRecord()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRecord(RecordInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            string imageUrl;
+            try
+            {
+                // Upload image to Cloudinary
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(input.Image.FileName, input.Image.OpenReadStream()),
+                    PublicId = $"{input.Holder}",
+                };
+
+                var uploadResult = await this.cloudinaryService.UploadPictureAsync(input.Image, $"{input.Holder}");
+                imageUrl = uploadResult;
+            }
+            catch (System.Exception)
+            {
+                // In case of missing Cloudinary configuration from appsettings.json
+                imageUrl = GlobalConstants.Images.CloudinaryMissing;
+            }
+
+            const int fixedCategoryId = 2;
+
+            await this.recordService.AddRecord(input.Id, input.Holder, input.Description, imageUrl, input.RecordTypeId, fixedCategoryId);
+            return this.RedirectToAction(nameof(this.Index));
+        }
+    }
+}
