b76ca36153607053afcd794998735e84ddfe32df
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-12-05 12:00:14 +0200

    lastMigration

---
+++ Data/BaseballStat.Data/Migrations/20241205054243_AddIsProtectedTeamandLeague.cs
@@ -0,0 +1,40 @@
+﻿#nullable disable
+
+namespace BaseballStat.Data.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class AddIsProtectedTeamandLeague : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsProtectedTeam",
+                table: "Teams",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsProtectedLeague",
+                table: "Leagues",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsProtectedTeam",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "IsProtectedLeague",
+                table: "Leagues");
+        }
+    }
+}
